16e69fb8e842dfaed36665db83c2cce9b3b4d0ee
Igor <igorsb3000@gmail.com>
2021-12-05 19:46:31 -0300

    Interface gráfica feita

---
+++ 2ªUnidade/Trem_-_Codigo_Fonte/Código QT5 Ubuntu - Trem/mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "trem.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+
+public slots:
+    void updateInterface(int,int,int);
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+
+    void on_horizontalSlider_valueChanged(int value);
+
+    void on_horizontalSlider_2_valueChanged(int value);
+
+    void on_horizontalSlider_3_valueChanged(int value);
+
+private:
+    Ui::MainWindow *ui;
+
+    //Cria os objetos TREM's
+    Trem *trem1;
+    Trem *trem2;
+    Trem *trem3;
+};
+
+#endif // MAINWINDOW_H
