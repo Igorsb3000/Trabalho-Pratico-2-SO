e5beacf12aada02a4903cb8d2380cf7cb0239f59
Wesley Gurgel <wesleygurgel27@gmail.com>
2021-12-08 17:23:56 -0300

    feat(adding_semaphores): still bugado kkkk

--- trem.cpp
+++ trem.cpp
@@ -10,7 +10,8 @@ QSemaphore semaforo6(1);
 QSemaphore semaforo7(1);
 
 //Construtor
-Trem::Trem(int ID, int x, int y){
+Trem::Trem(int ID, int x, int y)
+{
     this->ID = ID;
     this->x = x;
     this->y = y;
@@ -18,47 +19,228 @@ Trem::Trem(int ID, int x, int y){
 }
 
 // Setar velocidade.
-void Trem::setVelocidade(int velocidade){
-    if(velocidade <= 0){
+void Trem::setVelocidade(int velocidade)
+{
+    if (velocidade <= 0)
+    {
         this->velocidade = 0;
         return;
     }
-     this->velocidade = (int) 1000 / velocidade;
+    this->velocidade = (int)1000 / velocidade;
 }
 
 //Função a ser executada após executar trem->START
-void Trem::run(){
-    while(true){
-        if(this->velocidade == 0){
-            emit updateGUI(this->ID, this->x,this->y);
+void Trem::run()
+{
+    while (true)
+    {
+        if (this->velocidade == 0)
+        {
+            emit updateGUI(this->ID, this->x, this->y);
             continue;
         }
-        switch(ID){
+        switch (ID)
+        {
         case 1:
             //Trem 1
+            if (x == 310 && y == 30){
+                semaforo1.acquire();
+            }
 
-            emit updateGUI(ID, x,y);    //Emite um sinal
+            if (x ==  330 && y == 130){
+                semaforo4.acquire();
+            }
+
+            if (x == 310 && y == 150){
+                semaforo1.release();
+            }
+
+            if (x ==  150 && y == 150){
+                semaforo4.release();
+            }
+
+
+            // Movimentação do TREM 1
+            if (y == 30 && x < 330)
+                x += 10;
+            else if (x == 330 && y < 150)
+                y += 10;
+            else if (x > 60 && y == 150)
+                x -= 10;
+            else
+                y -= 10;
+            emit updateGUI(ID, x, y); //Emite um sinal
             break;
 
         case 2: //Trem 2
+            // Na porta de saída para entrar na VIA 2
+            if (x == 580 && y == 30){
+                semaforo2.acquire();
+            }
 
-            emit updateGUI(ID, x,y);    //Emite um sinal
+            // Quero sair da Via 2 e entrar na Via 7
+            if (x ==  600 && y == 130){
+                semaforo7.acquire();
+            }
+
+            // Estou na Via 7 e vou liberar a Via 2
+            if (x ==  580 && y == 150){
+                semaforo2.release();
+            }
+
+            // Quero sair da Via 7 e entrar na Via 6
+            if (x == 460 && y == 150){
+                semaforo6.acquire();
+            }
+
+            // Estou na via 6 e vou liberar a Via 7
+            if (x ==  420 && y == 150){
+                semaforo7.release();
+            }
+
+            // QUERO Sair da Via 6 e entrar na Via 1
+            if (x ==  350 && y == 150){
+                semaforo1.acquire();
+            }
+
+            // Estou na Via 1 e libero a Via 6
+            if (x ==  330 && y == 130){
+                semaforo6.release();
+            }
+
+            // Libero a Via 1
+            if (x ==  350 && y == 30){
+                semaforo1.release();
+            }
+
+
+            // Movimentação do TREM 2
+            if (y == 30 && x < 600)
+                x += 10;
+            else if (x == 600 && y < 150)
+                y += 10;
+            else if (x > 330 && y == 150)
+                x -= 10;
+            else
+                y -= 10;
+            emit updateGUI(ID, x, y); //Emite um sinal
             break;
 
         case 3: //Trem 3
 
-            emit updateGUI(ID, x,y);    //Emite um sinal
+            // Quero entrar na Via 3
+            if (x ==  730 && y == 150){
+                semaforo3.acquire();
+            }
+
+            // Estou na Via 3 e quero entrar na Via 2
+            if (x ==  620 && y == 150){
+                semaforo2.acquire();
+            }
+
+            // Estou na Via 2 e libero a Via 3
+            if (x ==  600 && y == 130){
+                semaforo3.release();
+            }
+
+            // Libero a Via 2
+            if (x ==  620 && y == 30){
+                semaforo2.release();
+            }
+
+            // Movimentaçãodo TREM 3
+            if (y == 30 && x < 870)
+                x += 10;
+            else if (x == 870 && y < 150)
+                y += 10;
+            else if (x > 600 && y == 150)
+                x -= 10;
+            else
+                y -= 10;
+
+            emit updateGUI(ID, x, y); //Emite um sinal
             break;
 
         case 4: //Trem 4
 
+            // Quero entrar na Via 4
+            if (x == 170 && y == 170){
+                semaforo4.acquire();
+            }
 
-            emit updateGUI(ID, x,y);    //Emite um sinal
+            // Estou na VIa 4 e quero entrar na Via 6
+            if (x == 310 && y == 150){
+                semaforo2.acquire();
+                semaforo6.acquire();
+            }
+
+            // Deixei a Via 4 e estou na Via 6
+            if (x == 350 && y == 150){
+                semaforo4.release();
+                semaforo2.release();
+            }
+
+            // Estou na VIa 6 e quero entrar na Via 5
+            if (x == 420 && y == 150){
+                semaforo5.acquire();
+            }
+
+            // Estou deixando a via 5
+            if (x == 420 && y == 280){
+                semaforo5.release();
+            }
+
+
+            // Movimentação do TREM 4
+            if (y == 150 && x < 440)
+                x += 10;
+            else if (x == 440 && y < 280)
+                y += 10;
+            else if (x > 170 && y == 280)
+                x -= 10;
+            else
+                y -= 10;
+
+            emit updateGUI(ID, x, y); //Emite um sinal
             break;
 
         case 5: //Trem 5
 
-            emit updateGUI(ID, x,y);    //Emite um sinal
+            // Quero entrar na Via 5
+            if (x == 460 && y == 280){
+                semaforo5.acquire();
+            }
+
+            // Estou na Via 5 e quero ir para a VIA 7
+            if (x == 440 && y == 170){
+                semaforo7.acquire();
+            }
+
+            // Deixando a Via 5
+            if(x == 460 && y == 150){
+                semaforo5.release();
+            }
+
+            // Estou na Via 7 e quero ir para a Via 3
+            if(x == 580 && y == 150){
+                semaforo3.acquire();
+            }
+
+            // Deixando a Via 3
+            if(x == 710 && y == 170){
+                semaforo3.acquire();
+            }
+
+            // Movimentação do TREM 5
+            if (y == 150 && x < 710)
+                x += 10;
+            else if (x == 710 && y < 280)
+                y += 10;
+            else if (x > 440 && y == 280)
+                x -= 10;
+            else
+                y -= 10;
+            emit updateGUI(ID, x, y); //Emite um sinal
             break;
 
         default:
@@ -67,7 +249,3 @@ void Trem::run(){
         msleep(velocidade);
     }
 }
-
-
-
-
